f08ea5a8c220647baba5ad509d08d216d9b47783
김정태 <tae101010@naver.com>
2022-03-11 20:57:20 +0900

    Initial Commit

---
+++ simpleNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simpleNote
+//
+//  Created by 김정태 on 2022/03/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
