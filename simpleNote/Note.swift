078746fc7cc164e6a831b4a1b9b40eefa4b9ea31
김정태 <tae101010@naver.com>
2022-03-12 21:44:12 +0900

    커스텀 테이블뷰(제목과 내용이 겹쳐서 나오는 문제 발생)

---
+++ simpleNote/Note.swift
@@ -0,0 +1,13 @@
+//
+//  Note.swift
+//  simpleNote
+//
+//  Created by 김정태 on 2022/03/12.
+//
+
+import Foundation
+
+struct Note {
+    var title: String
+    var content: String
+}
--- simpleNote/ViewController.swift
+++ simpleNote/ViewController.swift
@@ -8,13 +8,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    var note = [Note]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
-    //test
-
+    
+    @IBAction func addTapButton(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "메모", message: nil, preferredStyle: .alert)
+        
+       
+        
+        let registerButton = UIAlertAction(title: "등록", style: .default, handler: { [weak self] _ in
+            
+            guard let title = alert.textFields?[0].text else { return }
+            
+            guard let content = alert.textFields?[1].text else {return}
+            
+            let note = Note(title: title, content: content)
+            self?.note.append(note)
+            self?.tableView.reloadData()
+        })
+        
+        let cancelButton = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alert.addAction(registerButton)
+        alert.addAction(cancelButton)
+        
+        alert.addTextField(configurationHandler: { textfield1 in
+            textfield1.placeholder = "제목을 입력해주세요."
+        })
+        alert.addTextField(configurationHandler: { textfield2 in
+            textfield2.placeholder = "내용을 입력해주세요."
+        })
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            if indexPath.row == 1 {
+                return 200
+            }
+
+            return UITableView.automaticDimension
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        let note = self.note[indexPath.row]
+        
+        cell.titleLabel.text = note.title
+        cell.contentLabel.text = note.content
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.note.count
+    }
+}
